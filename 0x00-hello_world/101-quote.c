ed0d5af993e87f6c4801251a505ebce542f889dc
Ngarimwendwa <ngaricollins@gmail.com>
2023-03-16 13:18:16 -0700

    preprocessor

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#iclude <unistd.h>
+#include <unistd.h>
 
 /**
 * main - print quote
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
