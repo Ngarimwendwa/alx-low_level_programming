dc675178f5d234cca00716f5430713947c4a33a8
Ngarimwendwa <ngaricollins@gmail.com>
2023-03-16 13:42:00 -0700

    preprocessor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
 */
 
 int main(void)
-
+{
 	printf("size of a char: %ld byte(s)\n", sizeof(char));
 	printf("size of a int: %ld byte(s)\n", sizeof(int));
 	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
