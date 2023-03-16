a664c66826d97536097584b81210ba91160daaad
Ngarimwendwa <ngaricollins@gmail.com>
2023-03-16 13:15:59 -0700

    preprocessor

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#iclude <unistd.h>
+
+/**
+* main - print quote
+*
+* Description: print Dora's quote
+* Return: Always 1 (Success)
+*/
+
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,13 @@
 *
 * Your program should return 0
 */
+
 int main(void)
 {
 	char c;
 	int i;
-	long li;
-	long long lli;
+	long int li;
+	long long int lli;
 	float f;
 
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
